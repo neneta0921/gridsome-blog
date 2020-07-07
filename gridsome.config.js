60bbf02218821b647adebb5b0fd67c67c4f860c3
neneta0921 <info@neneta.net>
2020-07-07 22:40:57 +0900

    2020/07/07_fixed

---
+++ gridsome.config.js
@@ -0,0 +1,32 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Gridsome Blog',
+  siteDescription: 'A simple blog designed with Gridsome',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Post',
+        path: './content/posts/**/*.md',
+      }
+    }
+  ],
+  templates: {
+    Post: '/blog/:title',
+  },
+  transformers: {
+    //Add markdown support to all file-system sources
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      plugins: [
+        '@gridsome/remark-prismjs'
+      ]
+    }
+  },
+}
